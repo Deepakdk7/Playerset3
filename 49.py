9aa76ab8d81ec062cebbb7c3d8534ab1ad113fb9
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-12 23:07:22 +0530

    Update 49.py

--- 49.py
+++ 49.py
@@ -1,5 +1,7 @@
 ax=int(input())
-if ax<=9223372036854775807 and ax>=-9223372036854775808:
-    print('INT')
+if(ax>=-2**15+1  and ax<=2**15+1):
+    print ("INT")
+elif ax>=-2**31+1 and ax<=2**31+1:
+    print("LONG")  
 else:
-    print('LONG')
+    print("LONG LONG")
