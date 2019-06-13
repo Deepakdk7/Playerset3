76f64b20ca99d0cad23ca1588914742f3b73a11d
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-13 10:34:56 +0530

    Update 63.py

--- 63.py
+++ 63.py
@@ -4,8 +4,8 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 for i in range(0,ax):
     for j in range(0,ax):
-        if a[i]==b[j]:
+        if a[i]==b[j] and a[i] not in c:
             c.append(a[i])
-c=list(dict.fromkeys(c))
+#c=list(dict.fromkeys(c))
 for i in c:
     print(i,"",end="")
