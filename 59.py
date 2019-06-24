8e0ecb761470d2ad438b7c3571e1dce40b0efbc1
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-24 21:03:12 +0530

    Create 59.py

---
+++ 59.py
@@ -0,0 +1,13 @@
+ax=int(input())
+a=list(input().split())
+b=0
+c=[-1]
+d=[]
+for i in range(0,len(a)):
+    if a[i]=='0':
+        c.append(i)
+for i in range(0,len(c)-1):
+    d.append((c[i+1]-c[i])-1)
+b=sum(d)
+for i in range(0,b):
+    print('1',end=' ')
