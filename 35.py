c74bbca25115d45d94afc9c8f02f22022c1a878e
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-24 15:42:10 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,20 @@
+ax=list(input())
+a=[]
+b=[]
+d=[]
+for i in ax:
+    if i not in a:
+        a.append(i)
+a.remove(' ')
+for i in a:
+    c=0
+    for j in range(0,len(ax)):
+        if i==ax[j]:
+            c=c+1
+    b.append(i)
+    b.append(c)
+    d.append(c)
+x=min(d)
+for i in range(0,len(b)):
+    if x==b[i]:
+        print(b[i-1],end=' ')
