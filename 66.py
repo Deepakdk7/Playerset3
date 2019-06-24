67f1c8bca95d24ae5270023ce1cd5047f2da1e85
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-24 21:33:33 +0530

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,18 @@
+ax=input().split()
+k=int(ax[1])
+a=input().split()
+b=[]
+d=[]
+for i in a:
+    if i not in b:
+        b.append(i)
+for i in b:
+    c=0
+    for j in range(0,len(a)):
+        if i==a[j]:
+            c=c+1
+    d.append(i)
+    d.append(c)
+for i in range(0,len(d)):
+    if k==d[i]:
+        print(d[i-1])
